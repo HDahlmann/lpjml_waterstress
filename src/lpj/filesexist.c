efc99b19687ab044a39f08fcd2c302ba276452d1
Werner von Bloh <bloh@pik-potsdam.de>
2026-01-09 08:49:07 +0100

    File check of wateruse corrected in filesexist.c

--- src/lpj/filesexist.c
+++ src/lpj/filesexist.c
@@ -481,7 +481,7 @@ Bool filesexist(Config config, /**< LPJmL configuration */
     bad+=checkclmfile(&config,"tamp",&config.tamp_filename,NULL,LPJ_SHORT,TRUE);
   }
   if(config.wateruse)
-    bad+=checkdatafile(&config,&config.wateruse_filename,"wateruse","dm3/yr",LPJ_INT,1);
+    bad+=checkclmfile(&config,"wateruse",&config.wateruse_filename,NULL,LPJ_INT,FALSE);
   bad+=checkclmfile(&config,"temp",&config.temp_filename,"celsius",LPJ_SHORT,TRUE);
   bad+=checkclmfile(&config,"precipitation",&config.prec_filename,"kg/m2/day",LPJ_SHORT,TRUE);
 #ifdef IMAGE
